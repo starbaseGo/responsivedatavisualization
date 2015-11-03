b8b0bbc0da48923fd77c85759a6beea2afd78ff4
Bill Hinderman <bill@billhinderman.com>
2015-11-03 15:10:03 -0600

    ch05

--- exercises/chapter-05/exercise-3/answer.js
+++ exercises/chapter-05/exercise-3/answer.js
@@ -1,5 +1,17 @@
 (function() {
   $(document).ready(function() {
-    
+    var partials = [
+      {
+        "file":"/partial1.html",
+        "destination":"#aside-1"
+      },
+      {
+        "file":"/partial2.html",
+        "destination":"#aside-2"
+      }
+    ];
+    for(i=0; i<partials.length; i++) {
+      $(partials[i]['destination']).load(partials[i]['file']);
+    }
   });
 })();
